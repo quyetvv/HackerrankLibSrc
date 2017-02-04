c32066af01e3118e9d31da7f63b6db79a79aad97
Vu Van Quyet <quyetvv@gmail.com>
2017-02-04 09:17:08 +0700

    Create SumEvenNumber.cs

---
+++ SumEvenNumber.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] c_temp = Console.ReadLine().Split(' ');
+        int[] c = Array.ConvertAll(c_temp,Int32.Parse);
+        var count = c.GroupBy(x => x).Sum( y => y.Count() / 2);
+        Console.WriteLine(count);
+    }
+}
